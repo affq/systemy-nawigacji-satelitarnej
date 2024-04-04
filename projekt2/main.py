64234617107b765a1f6a0936b61873b8dd4e7af9
affq <adrianfabisiewicz@gmail.com>
2024-04-04 17:12:48 +0200

    bug fix

---
+++ projekt2/main.py
@@ -0,0 +1,23 @@
+from readrnx_studenci import readrnxnav, date2tow
+import numpy as np
+
+nav_file = r"projekt2\BRDC00WRD_R_20240650000_01D_GN.rnx"
+nav, inav = readrnxnav(nav_file)
+zdrowe = nav[:, 30] == 0
+nav = nav[zdrowe,:]
+inav = inav[zdrowe]
+
+
+# wejście
+data_obliczen = [2024, 3, 5, 11, 15, 0]
+week, tow, dow = date2tow(data_obliczen)
+
+sat = 2
+
+indeks_satelity = inav == sat
+nav_satelity = nav[indeks_satelity, :]
+
+dt = np.abs(tow - nav_satelity[:, 17])
+indeks_najmniejszej_roznicy = np.argmin(dt)
+
+nav_wybrane = nav_satelity[indeks_najmniejszej_roznicy, :]
